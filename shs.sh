c0a640ae229462e9e4540b0dfbf18f040959fe6f
Iain Lowe <iain.lowe@gmail.com>
2014-07-11 20:24:18 -0400

    added shs reload command

--- shs.sh
+++ shs.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
-ACTION=${1:-check}
+VERSION=`git tag -l | head -n1 || echo unreleased`
 
-echo OK
+source ~/.shs/lib/
+
+function shs() {
+	local cmd=$1
+	shift
+
+	case $cmd in
+		r|reload) shs_reload;;
+		*)
+			echo 'abort: unknown command' >&2
+			return 2
+			;;
+	esac
+}
+
+echo Shellscripts v$VERSION loaded
