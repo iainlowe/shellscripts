4c9835f65352390631583686df9dd00104ff2c37
Iain Lowe <iain.lowe@gmail.com>
2014-07-11 22:39:09 -0400

    moved cli library to shs

---
+++ lib/shs.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+function shs() {
+	local cmd=$1
+	shift
+
+	case $cmd in
+		up|update) _shs_update;;
+		v|version) _shs_version;;
+		*)
+			echo 'abort: unknown command' >&2
+			return 2
+			;;
+	esac
+}
+
+function _shs_version() {
+	local version=`git -C ~/.shs tag -l | head -n1`
+
+	echo "shs version ${version} (build `git -C ~/.shs log -n1 --oneline | cut -d' ' -f1`)"
+}
+
+function _shs_update() {
+	git -C ~/.shs pull
+	source ~/.bashrc
+}
+
+function _shs_unload() {
+	unset shs
+}
+
--- shs.sh
+++ shs.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-VERSION=`git -C ~/.shs tag -l | head -n1`
-[ -z "$VERSION" ] && VERSION=`git -C ~/.shs log -n1 --oneline | cut -d' ' -f1`
-
 # Bootstrap load require library
 source ~/.shs/lib/require.sh
 
 require colorecho
 require dirhooks
-require cli
+require shs
 
-echo Shellscripts v$VERSION loaded
+shs version
