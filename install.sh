a66f9a174b52f0ebd18432b0572cc740021bdcf7
Iain Lowe <iain.lowe@gmail.com>
2014-07-11 20:24:04 -0400

    added bashrc installer

--- install.sh
+++ install.sh
@@ -5,8 +5,6 @@ if [ -d ~/.shs ]; then
 	exit 5
 fi
 
-mkdir ~/.shs
-
 function check_bootstrap_deps() { # At a minimum we need a git command to start
 	if ! which git &>/dev/null; then
 		echo "No git executable found. Install git and try again."
@@ -15,3 +13,13 @@ function check_bootstrap_deps() { # At a minimum we need a git command to start
 }
 
 check_bootstrap_deps
+
+git clone http://github.com/ilowe/shellscripts ~/.shs
+
+echo '' >> ~/.bashrc
+cat <<EOF >> ~/.bashrc
+##########################################################################################
+#################### Added by github.com/ilowe/shellscripts installer ####################
+source ~/.shs/shs.sh
+##########################################################################################
+EOF
