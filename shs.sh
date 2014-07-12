fcd61172f758dbb8d77460de1c082bd96602ac13
Iain Lowe <iain.lowe@gmail.com>
2014-07-11 22:33:30 -0400

    show commit id for shs version

--- shs.sh
+++ shs.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 VERSION=`git -C ~/.shs tag -l | head -n1`
+[ -z "$VERSION" ] && VERSION=`git -C ~/.shs log -n1 --oneline | cut -d' ' -f1`
 
 # Bootstrap load require library
 source ~/.shs/lib/require.sh
